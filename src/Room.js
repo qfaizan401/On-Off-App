f73a498f66eee0f0189bb14f44479f98705164a3
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-23 03:40:15 +0500

    Update Room.js

--- src/Room.js
+++ src/Room.js
@@ -1,20 +1,16 @@
 import React , { useState } from 'react'
 
 const Room = () =>{
-    const [isLit, setLit] = useState(true)
+    const [roomLight, setLight] = useState(true)
     const [Age, setAge] = useState(20)
-    const ageBotton = () => setAge(Age+3)
-    const buttonFunc = () => (
-        console.log("Botton Clicked"),
-        setLit(!isLit)
-    )
+    const ageBotton = () => setAge(Age+1)
     return(
         <div>
-            Light: {isLit ? "light":"dark"}
+            Light: {roomLight ? "ON":"OFF"}
             <br/>
             Age : {Age}
             <br/>
-            <button onClick={buttonFunc}>Toggle Button</button>
+            <button onClick={ () => setLight(!roomLight) }>Toggle Button</button>
             <br/>
             <button onClick={ageBotton}>Age Botton</button>
         </div>
